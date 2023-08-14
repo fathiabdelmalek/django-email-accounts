b1c0e68831ccd9170d96b9a7921ed14cd5042e93
Fathi Abdelmalek <abdelmalek.fathi.2001@gmail.com>
2023-08-14 22:46:58 +0100

    change version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ extras_require = {
 
 setup(
     name='django-email-accounts',
-    version='0.3.1',
+    version='0.4.0',
     author='Fathi Abdelmalek',
     author_email='abdelmalek.fathi.2001@gmail.com',
     description='A Django app for user management with email-based authentication.',
